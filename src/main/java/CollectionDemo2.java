4ff3910593b15717e156355967de6112c0c7908b
ZhouZhipeng <1334093285@qq.com>
2021-07-05 15:56:03 +0800

    try some function in CollectionDemo2

--- src/main/java/CollectionDemo2.java
+++ src/main/java/CollectionDemo2.java
@@ -63,5 +63,18 @@ public class CollectionDemo2 {
         public String toString() {
             return "Student [name=" + name + ", age=" + age +"]";
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Student student = (Student) o;
+            return age == student.age && Objects.equals(name, student.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
     }
 }
