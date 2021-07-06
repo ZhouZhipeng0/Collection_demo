70e242618bc1149dc504a7d237d10a6563a35d43
ZhouZhipeng <1334093285@qq.com>
2021-07-06 16:32:55 +0800

    add new class StreamDemo and test

---
+++ src/main/java/StreamDemo.java
@@ -0,0 +1,9 @@
+/**
+ * @Author :13340
+ * @Date :Created in 2021-07-06-12:09
+ * @Description:
+ **/
+
+
+public class StreamDemo {
+}
---
+++ src/test/java/StreamDemoTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author :13340
+ * @Date :Created in 2021-07-06-12:09
+ * @Description:
+ **/
+
+public class StreamDemoTest {
+    /**
+     * 创建流
+     */
+    @Test
+    public void test01(){
+        /**
+         * 集合流
+         *  - Collection.stream() 穿行流
+         *  - Collection.parallelStream() 并行流
+         */
+        List<String> list = new ArrayList<>();
+        Stream<String> stream1 = list.stream();
+
+        //数组流
+        //Arrays.stream(array)
+        String[] strings = new String[10];
+        Stream<String> stream2 = Arrays.stream(strings);
+
+        //Stream 静态方法
+        //Stream.of(...)
+        Stream<Integer> stream3 = Stream.of(1, 2, 3);
+
+        //无限流
+        //迭代
+//        Stream<Integer> stream4 = Stream.iterate(0, (i) -> ++i+i++);
+//        stream4.forEach(System.out::println);
+
+        //生成
+        Stream.generate(() -> Math.random())
+                .limit(5)
+                .forEach(System.out::println);
+    }
+
+}
